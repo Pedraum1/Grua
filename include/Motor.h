07e8ebe27f7b0ba2419bbaef09f0b5dee74f565a
Pedraum1 <pedro.paulo60@aluno.ifce.edu.br>
2025-07-09 22:49:56 -0300

    FEATURE: Implementando classe Motor com representação digital do motor DC a ser controlado pela ponte H

---
+++ include/Motor.h
@@ -0,0 +1,28 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <Arduino.h>
+
+class Motor {
+    public:
+        Motor();
+        void atribuir(int pino1, int pino2, int pino3);
+        void ligar();
+        void desligar();
+        void ajustarVelocidade(int velocidade);
+        void ajustarSentido(bool sentido);
+        void trocarSentido();
+    
+    private:
+        bool ligado;
+        int velocidade;
+        bool sentido;
+        int pino_positivo;
+        int pino_negativo;
+        int pino_pwm;
+
+        // Função auxiliar para verificar se os pinos estão configurados corretamente
+        bool verificarPinos();
+};
+
+#endif
