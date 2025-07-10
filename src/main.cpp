e66196d16a8c0f60ba043db97c632a7ab5884a96
Pedraum1 <pedro.paulo60@aluno.ifce.edu.br>
2025-07-09 23:15:29 -0300

    FEATURE: Criando código básico para teste da classe Motor na protoboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include "Motor.h"
+
+Motor motor;
 
 void setup() {
-
+  motor.atribuir(6, 7, 10);   // Pinos do motor do cabo
 }
 
 void loop() {
+  motor.ligar();
 
+  motor.ajustarVelocidade(128); // Ajusta a velocidade do motor
+  delay(2000); // Mantém o motor ligado por 2 segundos
+
+  motor.desligar(); // Desliga o motor
+  delay(2000); // Espera 2 segundos antes de repetir
+  
+  motor.trocarSentido(); // Troca o sentido do motor
 }
