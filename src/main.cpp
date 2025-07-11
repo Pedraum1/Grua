565e7bf25c7564d0440aa40afa7f12708671cd42
Pedraum1 <pedro.paulo60@aluno.ifce.edu.br>
2025-07-11 18:27:56 -0300

    FEATURE: Adicionando mensagens de status para controle do motor

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,16 @@ Botao botao_mestre(4);
 Motor motor;
 
 void setup() {
+
+  Serial.begin(9600);
+  while(!Serial){}
+
+  Serial.println("Iniciando o controle do motor...");
+  
   motor.atribuir(6, 7, 10);
   motor.ajustarVelocidade(48);
   motor.ajustarSentido(HORARIO);
+
 }
 
 void loop() {
@@ -19,26 +26,32 @@ void loop() {
   if(botao_aumenta.clicado()){
     if(motor.sentidoAtual() == PARADO){
       motor.ajustarSentido(HORARIO);
+      Serial.println("Girando motor no sentido horário.");
     }
     if(motor.sentidoAtual() == ANTI_HORARIO){
       motor.ajustarSentido(PARADO);
+      Serial.println("Parando motor.");
     }
   }
 
   if(botao_diminui.clicado()){
     if(motor.sentidoAtual() == PARADO){
       motor.ajustarSentido(ANTI_HORARIO);
+      Serial.println("Girando motor no sentido anti-horário.");
     }
     if(motor.sentidoAtual() == HORARIO){
       motor.ajustarSentido(PARADO);
+      Serial.println("Parando motor.");
     }
   }
 
   if(botao_mestre.clicado()){
     if(motor.estaLigado()){
       motor.desligar();
+      Serial.println("Desligando motor.");
     } else {
       motor.ligar();
+      Serial.println("Ligando motor.");
     }
   }
 
